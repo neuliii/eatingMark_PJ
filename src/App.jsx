ce3d5ca6cacbc83a2c42b114da2be5bf93b8e65d
neuliii <phn01113@gmail.com>
2025-04-14 19:18:07 +0900

    📝 React-router 기본 초기 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { Link, Route, Routes } from "react-router"
+import { Main } from "./pages/Main"
+import { Detail } from "./pages/Detail"
+import { Favorite } from "./pages/Favorite"
 
 function App() {
 
 
   return (
     <>
-      
+      <nav>
+        <Link to={'/'}> Main </Link>
+        <Link to={'/datail'}> Detail </Link>
+        <Link to={'/favorite'}> Favorite </Link>
+      </nav>
+      <main>
+        <Routes>
+          <Route path={"/"} element={<Main />}/>
+          <Route path={"/detail/:placeId"} element={<Detail />}/>
+          <Route path={"/Favorite"} element={<Favorite />}/>
+        </Routes>
+      </main>
     </>
   )
 }
